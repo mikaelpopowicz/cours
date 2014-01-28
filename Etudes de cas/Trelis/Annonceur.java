2960ad6410fee809824812baef4b372cdab2196f
mik0_91 <mikael.popowicz@gmail.com>
2014-01-28 16:11:45 +0100

    Cours

--- Etudes de cas/Trelis/Annonceur.java
+++ Etudes de cas/Trelis/Annonceur.java
@@ -9,6 +9,25 @@ class Annonceur
 			{
 				messages.add(unMessage);
 			}
+			trierLesMessages(messages);
+		}
+	}
+	
+	public void trierLesMessages(LinkedList<Message> messagesATrier)
+	{
+		Message temp;
+		for(Messages unMessage : messagesATrier)
+		{
+			temp = unMessage;
+			if(unMessage.compare() == 1)
+			{
+				messagesATrier.set(messagesATrier.indexOf(unMessage), messagesATrier.get(messagesATrier.indexOf(unMessage)+1));
+				messagesATrier.set(messagesATrier.get(messagesATrier.indexOf(unMessage)+1), temp);
+			} else if (unMessage.commpare() == -1)
+			{
+				messagesATrier.set(messagesATrier.indexOf(unMessage), messagesATrier.get(messagesATrier.indexOf(unMessage)-1));
+				messagesATrier.set(messagesATrier.get(messagesATrier.indexOf(unMessage)-1), temp);
+			}
 		}
 	}
 }
